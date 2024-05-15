85407bf415996648f0a850cb660a2e1479131af5
Cally Lin <callyylin@gmail.com>
2024-05-14 21:53:46 -0700

    Add ability to run tests on multiple projects at once

--- geneExpressionTestingScript.py
+++ geneExpressionTestingScript.py
@@ -1,24 +1,30 @@
 import subprocess
 import sys
 
-if(len(sys.argv) != 2):
+if(len(sys.argv) < 2):
     print("Proper Usage: python3 geneExpressionTestingScript.py [Project Name]")
 
-projectName = sys.argv[1]
-typeDict = {'fpkm': True,
-            'fpkm_uq': True,
-            'tpm': True,
-            'star_counts': True}
+projects = sys.argv[1:]
+results = []
 
-typeToFile = { "fpkm": "star_fpkm",
-                "fpkm_uq": "star_fpkm-uq",
-                "tpm": "star_tpm",
-                "star_counts": "star_counts"
+for project in projects: 
+    typeDict = {'star_counts': True,
+                'tpm': True,
+                'fpkm': True,
+                'fpkm_uq': True,
                 }
-for type in typeDict:
-    try:
-        subprocess.run(["python3", "geneExpressionValidation.py", projectName, f"../{projectName}/Xena_Matrices/{projectName}.{typeToFile[type]}.tsv", type], check=True)
-    except subprocess.CalledProcessError:
-        typeDict[type] = False
+    typeToFile = {"star_counts": "star_counts",
+                  "tpm": "star_tpm",
+                  "fpkm": "star_fpkm",
+                  "fpkm_uq": "star_fpkm-uq",
+                }
+    for type in typeDict:
+        try:
+            subprocess.run(["python3", "geneExpressionValidation.py", project, f"../{project}/Xena_Matrices/{project}.{typeToFile[type]}.tsv", type], check=True)
+        except subprocess.CalledProcessError:
+            typeDict[type] = False
+    results.append([project, typeDict])
 
-print(typeDict)
+for result in results: 
+    print(f'{result[0]}: {result[1]}')
+    
